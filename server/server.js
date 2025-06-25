a8a42715fcc6280afe39a0b4259478cba375cd76
Abhijit Radhakrishnan <peringadanabhijit@gmail.com>
2025-06-25 18:11:26 +0530

    feat: setup Clerk authentication in backend

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB()
 
 const app = express();
 app.use(cors()) // Enable Cross-Origin Resource Sharing
+
+// Middleware 
+app.use(express.json())
+app.use(clerkMiddleware())
+
+// API to listen Clerk Webhooks 
+app.use("/api/clerk", clerkWebhooks)
 
 // 1st Route ( Main Route )
 app.get('/', (req, res)=> res.send("API is working"))
